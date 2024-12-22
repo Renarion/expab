473d17f4628df6f1447a079dc171e1a27ca3f69f
eeeeegor <53909149+eeeeegor@users.noreply.github.com>
2024-12-22 20:17:35 +0200

    Update setup.py
    upd version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AB_library",
-    version="0.2.4",
+    version="0.2.5",
     packages=find_packages(),
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
