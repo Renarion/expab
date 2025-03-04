c3d1f20443fa7c2484f63f427eec1a64a940126a
Renat <113312541+Renarion@users.noreply.github.com>
2025-03-04 20:01:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="AB_library",
+    name="expab",
     version="0.0.1",
     url='https://github.com/Renarion/AB_library',
     packages=find_packages(),
